f553428c68a904a5e2ad9efefff95a73df109bc5
Anthony Bosek <anthony.bosek@gmail.com>
2023-11-15 14:12:44 -0700

    complete lab

--- server/models.py
+++ server/models.py
@@ -3,8 +3,9 @@ from sqlalchemy_serializer import SerializerMixin
 
 db = SQLAlchemy()
 
+
 class Message(db.Model, SerializerMixin):
-    __tablename__ = 'messages'
+    __tablename__ = "messages"
 
     id = db.Column(db.Integer, primary_key=True)
     body = db.Column(db.String)
@@ -13,4 +14,4 @@ class Message(db.Model, SerializerMixin):
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
     def __repr__(self):
-        return f'<Message by {self.username}: {self.body[:10]}...>'
+        return f"<Message by {self.username}: {self.body[:10]}...>"
